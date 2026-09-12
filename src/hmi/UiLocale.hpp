54e730f27af016dc84a2533aeb9a3400f247396b
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-12 08:18:47 -0700

    Add ten-language UI locale model

--- src/hmi/UiLocale.hpp
+++ src/hmi/UiLocale.hpp
@@ -4,11 +4,23 @@
 
 namespace nexvary::avionics {
 
-enum class UiLanguage { English, Arabic };
+enum class UiLanguage {
+    English,
+    Arabic,
+    Turkish,
+    Spanish,
+    German,
+    Italian,
+    French,
+    Urdu,
+    Persian,
+    Russian
+};
 
 class UiLocale {
 public:
     [[nodiscard]] static UiLanguage fromCode(std::string_view code) noexcept;
+    [[nodiscard]] static std::string code(UiLanguage language);
     [[nodiscard]] static bool isRtl(UiLanguage language) noexcept;
     [[nodiscard]] static std::string text(UiLanguage language, std::string_view key);
 };
